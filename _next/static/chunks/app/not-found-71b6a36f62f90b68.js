3139c87e5daa7ec914fa4428bd10fbcb9831738d
Me <40895217+juniorfred@users.noreply.github.com>
2024-02-08 21:09:48 +0100

    Add files via upload

---
+++ _next/static/chunks/app/not-found-71b6a36f62f90b68.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{5791:function(n,e,o){Promise.resolve().then(o.t.bind(o,6737,23))},6737:function(n){n.exports={main:"not-found_main__0NPrd"}}},function(n){n.O(0,[971,938,744],function(){return n(n.s=5791)}),_N_E=n.O()}]);
